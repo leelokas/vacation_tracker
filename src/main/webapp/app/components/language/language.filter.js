c18d2e3d7914b72069417a64fda3e222266afbc6
leelokas <leelo.rand@gmail.com>
2016-09-18 19:52:34 +0300

    #3 added Sign in and Create Vacation buttons to home page, hided some unnecessary navbar options

--- src/main/webapp/app/components/language/language.filter.js
+++ src/main/webapp/app/components/language/language.filter.js
@@ -10,19 +10,8 @@
 
         function findLanguageFromKeyFilter(lang) {
             return {
-                'da': 'Dansk',
-                'de': 'Deutsch',
                 'en': 'English',
-                'es': 'Español',
-                'fr': 'Français',
-                'gl': 'Galego',
-                'hu': 'Magyar',
-                'it': 'Italiano',
-                'nl': 'Nederlands',
-                'pl': 'Polski',
-                'ro': 'Română',
-                'ru': 'Русский',
-                'sk': 'Slovenský',
+                'sv': 'Svenska'
             }[lang];
         }
     }
