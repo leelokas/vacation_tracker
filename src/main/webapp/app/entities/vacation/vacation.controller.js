e2f3313c32377a6f64d6331e406b2fcd929371e3
leelokas <leelo.rand@gmail.com>
2016-09-27 00:25:17 +0300

    #18 "Sent" stage

--- src/main/webapp/app/entities/vacation/vacation.controller.js
+++ src/main/webapp/app/entities/vacation/vacation.controller.js
@@ -5,16 +5,17 @@
         .module('vacationTrackerApp')
         .controller('VacationController', VacationController);
 
-    VacationController.$inject = ['$scope', '$state', 'Vacation', 'ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants'];
+    VacationController.$inject = ['$scope', '$state', '$filter', 'Vacation', 'ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants'];
 
-    function VacationController ($scope, $state, Vacation, ParseLinks, AlertService, pagingParams, paginationConstants) {
+    function VacationController ($scope, $state, $filter, Vacation, ParseLinks, AlertService, pagingParams, paginationConstants) {
         var vm = this;
-        
+
         vm.loadPage = loadPage;
         vm.predicate = pagingParams.predicate;
         vm.reverse = pagingParams.ascending;
         vm.transition = transition;
         vm.itemsPerPage = paginationConstants.itemsPerPage;
+        vm.confirm = confirm;
 
         loadAll();
 
@@ -53,6 +54,22 @@
                 page: vm.page,
                 sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
                 search: vm.currentSearch
+            });
+        }
+
+        function confirm (vacation) {
+            vacation.stage = "SENT";
+            Vacation.update(vacation, function (result) {
+                loadAll();
+                AlertService.info("vacationTrackerApp.vacation.sent", {
+                    vacation: {
+                        startDate: $filter('date')(new Date(result.startDate), "dd/MM/yyyy"),
+                        endDate: $filter('date')(new Date(result.endDate), "dd/MM/yyyy")
+                    },
+                    manager: { //TODO task #14
+                        firstName: "John",
+                        lastName: "Doe"
+                    }});
             });
         }
     }
