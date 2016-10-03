95217c054917167d1bfe0039a645e4c91c96f0e2
leelokas <leelo.rand@gmail.com>
2016-10-04 00:01:44 +0300

    #32 fixed compilation error

--- src/test/java/com/reach_u/vacation/web/rest/AccountResourceIntTest.java
+++ src/test/java/com/reach_u/vacation/web/rest/AccountResourceIntTest.java
@@ -378,7 +378,8 @@ public class AccountResourceIntTest {
             "funky@example.com",    // e-mail
             true,                   // activated
             "en",                   // langKey
-            new HashSet<>(Arrays.asList(AuthoritiesConstants.USER))
+            new HashSet<>(Arrays.asList(AuthoritiesConstants.USER)),
+            null
         );
 
         restUserMockMvc.perform(
