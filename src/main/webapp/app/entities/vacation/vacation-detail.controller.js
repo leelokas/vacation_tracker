82f7ec69137f9beeef441f688d88d3a72bc1d0eb
leelokas <leelo.rand@gmail.com>
2016-10-16 23:27:39 +0300

    #49 fix

--- src/main/webapp/app/entities/vacation/vacation-detail.controller.js
+++ src/main/webapp/app/entities/vacation/vacation-detail.controller.js
@@ -5,13 +5,22 @@
         .module('vacationTrackerApp')
         .controller('VacationDetailController', VacationDetailController);
 
-    VacationDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Vacation', 'User'];
+    VacationDetailController.$inject = ['$scope', '$rootScope', 'previousState', 'entity', 'User', 'Principal'];
 
-    function VacationDetailController($scope, $rootScope, $stateParams, previousState, entity, Vacation, User) {
+    function VacationDetailController($scope, $rootScope, previousState, entity, User, Principal) {
         var vm = this;
 
         vm.vacation = entity;
         vm.previousState = previousState.name;
+        vm.currentUser = null;
+
+        setCurrentUser();
+
+        function setCurrentUser() {
+            Principal.identity().then(function(account) {
+                vm.currentUser = User.get({login: account.login});
+            });
+        }
 
         var unsubscribe = $rootScope.$on('vacationTrackerApp:vacationUpdate', function(event, result) {
             vm.vacation = result;
