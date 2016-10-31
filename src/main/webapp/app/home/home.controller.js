1015c66231b3a17044661efca0ac600fec558314
leelokas <leelo.rand@gmail.com>
2016-10-31 21:45:30 +0200

    #10 #11 #25

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,20 +5,27 @@
         .module('vacationTrackerApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$rootScope', '$scope', 'Principal', '$timeout', '$state', 'Auth'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($rootScope, $scope, Principal, $timeout, $state, Auth) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
-        vm.login = LoginService.open;
-        vm.register = register;
+        vm.authenticationError = false;
+        vm.credentials = {};
+        vm.login = login;
+        vm.password = null;
+        vm.rememberMe = true;
+        vm.username = null;
+
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
 
         getAccount();
+
+        $timeout(function (){angular.element('#username').focus();});
 
         function getAccount() {
             Principal.identity().then(function(account) {
@@ -26,8 +33,35 @@
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
         }
-        function register () {
-            $state.go('register');
+
+        function login (event) {
+            event.preventDefault();
+            Auth.login({
+                username: vm.username,
+                password: vm.password,
+                rememberMe: vm.rememberMe
+            }).then(function () {
+                vm.authenticationError = false;
+                if ($state.current.name === 'register' || $state.current.name === 'activate' ||
+                    $state.current.name === 'finishReset' || $state.current.name === 'requestReset') {
+                    $state.go('home');
+                }
+
+                $rootScope.$broadcast('authenticationSuccess');
+
+                // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+                // since login is succesful, go to stored previousState and clear previousState
+                if (Auth.getPreviousState()) {
+                    var previousState = Auth.getPreviousState();
+                    Auth.resetPreviousState();
+                    $state.go(previousState.name, previousState.params);
+                }
+
+                $state.go('vacation');
+            }).catch(function () {
+                vm.authenticationError = true;
+            });
         }
+
     }
 })();
--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -24,6 +24,7 @@
             resolve: {
                 mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
                     $translatePartialLoader.addPart('home');
+                    $translatePartialLoader.addPart('login');
                     return $translate.refresh();
                 }]
             }
