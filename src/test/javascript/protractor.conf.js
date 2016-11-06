5a348869739f207180c5205607fbf85982ac1930
lucyhilltop <liisupzz@gmail.com>
2016-11-06 18:47:13 +0200

    #5

--- src/test/javascript/protractor.conf.js
+++ src/test/javascript/protractor.conf.js
@@ -7,9 +7,9 @@ const prefix = 'src/test/javascript/'.replace(/[^/]+/g,'..');
 
 var webbrowserDriver= '';
 if (os.platform() === 'win32') {
-    webbrowserDriver = prefix + 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.24.exe';
+    webbrowserDriver = prefix + 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.25.exe';
 } else {
-    webbrowserDriver = prefix + 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.24.exe';
+    webbrowserDriver = prefix + 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.25exe';
 }
 
 exports.config = {
