a4a4c9b4dc6865f197dc2dc233891abeab417be3
lucyhilltop <liisupzz@gmail.com>
2016-11-07 21:22:13 +0200

    #77 Code style fixes

--- src/test/javascript/e2e/entities/vacation.js
+++ src/test/javascript/e2e/entities/vacation.js
@@ -2,140 +2,92 @@
 
 describe('E2e tests', function () {
 
-    var username = element(by.id('username'));
-    var password = element(by.id('password'));
-
-    var login = element(by.id('login'));
-    var logout = element(by.id('logout2'));
-
-    var startDate = element(by.id('field_startDate'));
-    var endDate = element(by.id('field_endDate'));
-
-    var submit = element(by.css('button[type=submit]'));
-    var addNew = element(by.css('[ui-sref="vacation.new"]'));
-
-    var vacationTypeColumn = element(by.xpath('//table/tbody/tr[last()]/td[3]'));
-    var vacationStageColumn = element(by.xpath('//table/tbody/tr[last()]/td[4]'));
-    var vacationPaymentColumn = element(by.xpath('//table/tbody/tr[last()]/td[5]'));
-    var vacationButtonColumn = element(by.xpath('//table/tbody/tr[last()]/td[6]'));
-    var vacationDateColumn = element(by.xpath('//table/tbody/tr[last()]/td[1]'));
-
-    var date = new Date();
-    date.setDate(date.getDate() + 1);
-    var dateString = ("0" + date.getDate()).slice(-2) + "/" + ("0" + date.getMonth()).slice(-2) + "/" + date.getFullYear();
+    var h = require('./helperFunctions.js');
 
     it('should log in with user user', function () {
-        browser.get('/');
-        username.sendKeys('user');
-        password.sendKeys('user');
-        submit.click();
+        h.login('user','user');
     });
 
+
+
     it('should load Vacations', function () {
-        element.all(by.css('[ui-sref="vacation"]')).first().click().then(function() {
-            element.all(by.css('h2')).first().getAttribute('data-translate').then(function (value) {
-                expect(value).toMatch(/vacationTrackerApp.vacation.home.title/);
-            });
-        });
+        h.checkTitle('[ui-sref="vacation"]', 'h2', /vacationTrackerApp.vacation.home.title/);
     });
 
     it('should load create Vacation dialog', function () {
-        element(by.css('[ui-sref="vacation.new"]')).click().then(function() {
-            element(by.css('h4.modal-title')).getAttribute('data-translate').then(function (value) {
-                expect(value).toMatch(/vacationTrackerApp.vacation.home.createOrEditLabel/);
-            });
+            h.checkTitle('[ui-sref="vacation.new"]','h4.modal-title',/vacationTrackerApp.vacation.home.createOrEditLabel/);
             element(by.css('button.close')).click();
-        });
     });
 
     it('should create a vacation', function () {
-        addNew.click().then(function () {
-            startDate.sendKeys(dateString);
-            endDate.sendKeys(dateString);
-            submit.click();
+        h.addNew.click().then(function () {
+            h.startDate.sendKeys(h.dateString);
+            h.endDate.sendKeys(h.dateString);
+            h.submit.click();
             });
-        vacationTypeColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.VacationType.PAID/);
-        });
-        vacationStageColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.Stage.SAVED/);
-        });
-        vacationPaymentColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.PaymentType.BEFORE_VACATION/);
-        });
+        h.checkRequestData(h.vacationTypeColumn,/vacationTrackerApp.VacationType.PAID/);
+        h.checkRequestData(h.vacationStageColumn,/vacationTrackerApp.Stage.SAVED/);
+        h.checkRequestData(h.vacationPaymentColumn,/vacationTrackerApp.PaymentType.BEFORE_VACATION/);
         });
 
 
     it('should send a vacation', function () {
-        vacationButtonColumn.element(by.buttonText('Send to confirmation')).click();
-        vacationTypeColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.VacationType.PAID/);
-        });
-        vacationStageColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.Stage.SENT/);
-        });
-        vacationPaymentColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.PaymentType.BEFORE_VACATION/);
-        });
-        expect(vacationButtonColumn.element(by.buttonText('Cancel request')).isPresent()).toBe(true);
-        expect(vacationButtonColumn.element(by.buttonText('Delete')).isPresent()).toBe(false);
+        h.clickButton(h.vacationButtonColumn,'Send to confirmation');
+        h.checkRequestData(h.vacationTypeColumn,/vacationTrackerApp.VacationType.PAID/);
+        h.checkRequestData(h.vacationStageColumn,/vacationTrackerApp.Stage.SENT/);
+        h.checkRequestData(h.vacationPaymentColumn,/vacationTrackerApp.PaymentType.BEFORE_VACATION/);
+        expect(h.vacationButtonColumn.element(by.buttonText('Cancel request')).isPresent()).toBe(true);
+        expect(h.vacationButtonColumn.element(by.buttonText('Delete')).isPresent()).toBe(false);
     });
 
 
     it('should make a sick leave request',function() {
-        addNew.click().then(function () {
+        h.addNew.click().then(function () {
             element(by.id('field_type')).sendKeys('Sick leave');
-            startDate.sendKeys(dateString);
+            h.startDate.sendKeys(h.dateString);
             expect(element(by.id('field_payment')).isEnabled()).toBe(false);
             expect(element(by.id('field_endDate')).isEnabled()).toBe(false);
-            submit.click();
+            h.submit.click();
         });
-        vacationTypeColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.VacationType.SICK_LEAVE/);
-        });
-      /*  vacationPaymentColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.PaymentType.WITH_NEXT_SALARY/);
-        });
-        */
-        vacationButtonColumn.element(by.buttonText('Send to confirmation')).click();
-        vacationStageColumn.getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.Stage.CONFIRMED/);
-        });
-        expect(vacationButtonColumn.element(by.buttonText('End date')).isPresent()).toBe(true);
+        h.checkRequestData(h.vacationTypeColumn,/vacationTrackerApp.VacationType.SICK_LEAVE/);
+        h.checkRequestData(h.vacationPaymentColumn,/vacationTrackerApp.PaymentType.WITH_NEXT_SALARY/);
+
+        h.clickButton(h.vacationButtonColumn,'Send to confirmation');
+
+        h.checkRequestData(h.vacationStageColumn,/vacationTrackerApp.Stage.CONFIRMED/);
+
+        expect(h.vacationButtonColumn.element(by.buttonText('End date')).isPresent()).toBe(true);
     });
 
     it('should add end date to sick leave request', function() {
-        vacationButtonColumn.element(by.buttonText('End date')).click();
+        h.clickButton(h.vacationButtonColumn,'End date');
         expect(element(by.id('field_startDate_sickLeave')).isEnabled()).toBe(false);
-        element(by.id('field_endDate_sickLeave')).sendKeys(dateString);
-        submit.click();
-        vacationDateColumn.getText().then(function(text) {
-            expect(text).toMatch(dateString + ' - ' + dateString);
+        element(by.id('field_endDate_sickLeave')).sendKeys(h.dateString);
+        h.submit.click();
+        h.vacationDateColumn.getText().then(function(text) {
+            expect(text).toMatch(h.dateString + ' - ' + h.dateString);
         });
-        expect(vacationButtonColumn.element(by.buttonText('End date')).isPresent()).toBe(false);
-        expect(vacationButtonColumn.element(by.buttonText('Cancel request')).isPresent()).toBe(true);
-        expect(vacationButtonColumn.element(by.buttonText('Delete')).isPresent()).toBe(false);
+        expect(h.vacationButtonColumn.element(by.buttonText('End date')).isPresent()).toBe(false);
+        expect(h.vacationButtonColumn.element(by.buttonText('Cancel request')).isPresent()).toBe(true);
+        expect(h.vacationButtonColumn.element(by.buttonText('Delete')).isPresent()).toBe(false);
 
     });
 
     it('should make an extra vacation request', function() {
-        addNew.click().then(function () {
-            startDate.sendKeys(dateString);
-            endDate.sendKeys(dateString);
-            submit.click();
+        h.addNew.click().then(function () {
+            h.startDate.sendKeys(h.dateString);
+            h.endDate.sendKeys(h.dateString);
+            h.submit.click();
         });
-        vacationButtonColumn.element(by.buttonText('Send to confirmation')).click();
+        h.clickButton(h.vacationButtonColumn,'Send to confirmation');
     });
 
     it( 'should log out with user user', function () {
-        logout.click();
+        h.logout.click();
     });
 
     it('should log in with manager/admin user', function() {
-        browser.get('/');
-        username.sendKeys('admin');
-        password.sendKeys('admin');
-        submit.click();
+        h.login('admin','admin');
     });
 
     it('should confirm request', function () {
@@ -143,14 +95,11 @@ describe('E2e tests', function () {
             element.all(by.xpath('//table/tbody/tr')).count().then(function(originalCount) {
                 var requests = originalCount;
 
-                element.all(by.css('[ui-sref="manager"]')).first().click().then(function() {
-                    element.all(by.css('h2')).first().getAttribute('data-translate').then(function (value) {
-                        expect(value).toMatch(/vacationTrackerApp.vacation.manager.title/);
-                    });
-                });
+                h.checkTitle('[ui-sref="manager"]','h2',/vacationTrackerApp.vacation.manager.title/);
+
                 element.all(by.xpath('//table/tbody/tr')).count().then(function(originalCount) {
                     var requests2 = originalCount;
-                    element(by.xpath('//table/tbody/tr[last()]/td[5]')).element(by.buttonText('Confirm')).click();
+                    h.clickButton(element(by.xpath('//table/tbody/tr[last()]/td[5]')),'Confirm');
                     var new_requests2 = element.all(by.xpath('//table/tbody/tr')).count();
                     expect(new_requests2).toBe(requests2 - 1);
                 });
@@ -171,7 +120,7 @@ describe('E2e tests', function () {
                 element.all(by.css('[ui-sref="manager"]')).first().click();
                 element.all(by.xpath('//table/tbody/tr')).count().then(function(originalCount) {
                     var requests2 = originalCount;
-                    element(by.xpath('//table/tbody/tr[last()]/td[5]')).element(by.buttonText('Reject')).click();
+                    h.clickButton(element(by.xpath('//table/tbody/tr[last()]/td[5]')),'Reject');
                     var new_requests2 = element.all(by.xpath('//table/tbody/tr')).count();
                     expect(new_requests2).toBe(requests2 - 1);
                 });
@@ -217,26 +166,22 @@ describe('E2e tests', function () {
     */
 
     it('should log out with manager/admin user', function() {
-        logout.click();
+        h.logout.click();
     });
 
     it('should log in with user user', function () {
-        browser.get('/');
-        username.sendKeys('user');
-        password.sendKeys('user');
-        submit.click();
+        h.login('user','user');
     });
 
 
     it('should check that the request was rejected (i.e in SAVED state now) and delete the request', function() {
         element.all(by.css('[ui-sref="vacation"]')).first().click();
-        element(by.xpath('//table/tbody/tr[last()-2]/td[4]')).getAttribute('data-translate').then(function(value) {
-            expect(value).toMatch(/vacationTrackerApp.Stage.SAVED/);
-        });
+
+        h.checkRequestData(element(by.xpath('//table/tbody/tr[last()-2]/td[4]')),/vacationTrackerApp.Stage.SAVED/);
+
         element.all(by.xpath('//table/tbody/tr')).count().then(function(originalCount) {
             var requests = originalCount;
-            element(by.xpath('//table/tbody/tr[last()-2]/td[6]')).element(by.buttonText('Delete')).click();
-            element(by.id('deleteForm')).submit();
+            h.clickAndSubmit(element(by.xpath('//table/tbody/tr[last()-2]/td[6]')),'Delete','deleteForm');
             var new_requests = element.all(by.xpath('//table/tbody/tr')).count();
             expect(new_requests).toBe(requests - 1);
         });
@@ -246,15 +191,10 @@ describe('E2e tests', function () {
         element.all(by.css('[ui-sref="overview"]')).first().click().then(function() {
             element.all(by.xpath('//table/tbody/tr')).count().then(function(originalCount) {
                 var requests = originalCount;
-
                 element.all(by.css('[ui-sref="vacation"]')).first().click().then(function() {
-                    vacationButtonColumn.element(by.buttonText('Cancel request')).click();
-                    element(by.id('cancelForm')).submit();
-                    vacationStageColumn.getAttribute('data-translate').then(function(value) {
-                        expect(value).toMatch(/vacationTrackerApp.Stage.SAVED/);
-                    });
+                    h.clickAndSubmit(h.vacationButtonColumn,'Cancel request','cancelForm');
+                    h.checkRequestData(h.vacationStageColumn,/vacationTrackerApp.Stage.SAVED/);
                 });
-
                 element.all(by.css('[ui-sref="overview"]')).first().click().then(function(){
                     var new_requests = element.all(by.xpath('//table/tbody/tr')).count();
                     expect(new_requests).toBe(requests - 1);
@@ -267,12 +207,9 @@ describe('E2e tests', function () {
         element.all(by.css('[ui-sref="vacation"]')).first().click();
         element.all(by.xpath('//table/tbody/tr')).count().then(function(originalCount) {
             var requests = originalCount;
-            vacationButtonColumn.element(by.buttonText('Delete')).click();
-            element(by.id('deleteForm')).submit();
-            vacationButtonColumn.element(by.buttonText('Cancel request')).click();
-            element(by.id('cancelForm')).submit();
-            vacationButtonColumn.element(by.buttonText('Delete')).click();
-            element(by.id('deleteForm')).submit();
+            h.clickAndSubmit(h.vacationButtonColumn,'Delete','deleteForm');
+            h.clickAndSubmit(h.vacationButtonColumn,'Cancel request','cancelForm');
+            h.clickAndSubmit(h.vacationButtonColumn,'Delete','deleteForm');
             var new_requests = element.all(by.xpath('//table/tbody/tr')).count();
             expect(new_requests).toBe(requests - 2);
         });
